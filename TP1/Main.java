0b59c12e135916c87a5957f041247593d8e25f4b
Thais Andreatta <89612369+thaisand@users.noreply.github.com>
2023-04-28 19:16:26 -0300

    Update Main.java

--- TP1/Main.java
+++ TP1/Main.java
@@ -3,49 +3,37 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // Cria uma instância da classe Random para gerar números aleatórios
-        Random rand = new Random();
-        // Define um array com os tamanhos dos grafos a serem gerados
-        int[] tamanhos = {100, 1000, 4000};
-        // Define o número de vezes que cada tamanho de grafo será gerado e testado
-        int tentativas = 10;
+        Random rand = new Random(); // Gera numeros aleatorios   
+        int[] tamanhos = {100, 1000, 4000}; // Tamanhos dos grafos a serem gerados       
+        int tentativas = 10; // Numero de vezes que cada tamanho de grafo sera gerado e testado
         
-        // Loop for para percorrer cada tamanho de grafo
         for (int tamanho : tamanhos) {
             System.out.println("----------------------------------------------------");
-            // Exibe uma mensagem informando o tamanho do grafo atual
             System.out.println("Grafo tamanho " + tamanho);
-            // Inicializa a variável tempoTotal como 0
             double tempoTotal = 0;
             
-            // Loop for para gerar e testar o grafo aleatório "tentativas" vezes
+            // Gera e testa o grafo aleatório 
             for (int i = 0; i < tentativas; i++) {
-                // Gera um novo grafo aleatório com o tamanho especificado e o objeto Random
                 Grafo g = geraGrafo(tamanho, rand);
                 
-                // Executa o algoritmo Tarjan para encontrar os componentes biconexos do grafo g
-                // Mede o tempo que leva para executar o algoritmo usando a classe System.nanoTime()
+                // Executa o algoritmo Tarjan e mede o tempo de execucao
                 long inicio = System.nanoTime();
                 Tarjan tarjan = new Tarjan(g);
                 tarjan.getComponentesBiconexos();
-                long fim = System.nanoTime();
-                // Calcula o tempo que levou para executar o algoritmo em milissegundos
-                double tempo = (fim - inicio) / 1000000.0;
-                // Adiciona o tempo atual ao tempo total
+                long fim = System.nanoTime();               
+                double tempo = (fim - inicio) / 1000000.0; // Calcula o tempo que levou para executar o algoritmo em milissegundos
                 tempoTotal += tempo;
             }           
             // Calcula o tempo médio que levou para executar o algoritmo em cada grafo do tamanho atual
             double tempoMedio = tempoTotal / tentativas;
-            // Exibe o tempo médio na tela
             System.out.println("Tempo médio Tarjan: " + tempoMedio + " ms");
         }
     }
     
-    // Método para gerar um novo grafo aleatório com o tamanho especificado
+    // Gera um novo grafo aleatório com o tamanho especificado
     public static Grafo geraGrafo(int tamanho, Random rand) {
-        // Cria uma nova instância da classe Grafo com o tamanho especificado
         Grafo g = new Grafo(tamanho);
-        // Loop for para percorrer cada par de vértices do grafo
+        // Percorre cada par de vértices do grafo
         for (int i = 0; i < tamanho; i++) {
             for (int j = i + 1; j < tamanho; j++) {
                 // Adiciona uma aresta entre os vértices i e j com probabilidade de 50%
@@ -54,7 +42,6 @@ public class Main {
                 }
             }
         }
-        // Retorna o grafo gerado
         return g;
     }
 }
